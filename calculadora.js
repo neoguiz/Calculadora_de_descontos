be2a9298745bc2bd526f1c469ea5f6eaaee1cae9
Guilherme D. Barbosa <90660406+neoguiz@users.noreply.github.com>
2022-05-10 09:33:42 -0300

    Added code to calculator script

---
+++ calculadora.js
@@ -0,0 +1,28 @@
+
+var Section = document.getElementById('content')
+var Input_pre = document.getElementById('Valor_pre')
+var Input_desc = document.getElementById('Valor_des')
+var Submit = document.getElementById('Submit_button')
+
+Submit.addEventListener('click', function(e){
+
+    var Valor_pre = Input_pre.value
+    var Valor_des = Input_desc.value
+
+    console.log(`Valor preço = ${Valor_pre}`)
+    console.log(`Valor desconto = ${Valor_des}`)
+
+    const Porc_total = 100
+    var Porc_aplicada = 100 - Valor_des
+
+    var Valor_original = (Valor_pre * Porc_aplicada) / Porc_total
+
+    var Div_resultado = document.createElement("div")
+    var Div_texto = document.createTextNode(`O valor do preço
+    antes da aplicação do desconto era de ${Valor_original}`)
+
+    Div_resultado.appendChild(Div_texto)
+    Section.appendChild(Div_resultado)
+
+})
+
