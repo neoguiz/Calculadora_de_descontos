f6b339f60aaf72b39c6cfb0ab66fe3be5930a6aa
Guilherme D. Barbosa <gui.diasbarbosa2003@gmail.com>
2022-05-13 15:23:50 -0300

    Solved appending problem
    Still need to finish VerificaValorNulo() fuction

--- calculadora.js
+++ calculadora.js
@@ -6,17 +6,12 @@ var Submit = document.getElementById('Submit_button')
 
 Submit.addEventListener('click', function(e){
 
-    
-    if (Section.contains(Div_Resultado)){
-        Div_resultado.remove()
-        console.log("Elemento retirado")
-    }
+
+    Div_sobrando = document.getElementById("Div_resultado")
+    EliminaDivSobrando(Div_sobrando)
 
     var Valor_pre = Input_pre.value
     var Valor_des = Input_desc.value
-
-    console.log(`Valor preço = ${Valor_pre}`)
-    console.log(`Valor desconto = ${Valor_des}`)
 
     const Porc_total = 100
     var Porc_aplicada = 100 - Valor_des
@@ -24,12 +19,35 @@ Submit.addEventListener('click', function(e){
     var Valor_original = (Valor_pre * Porc_total) / Porc_aplicada
 
     var Div_resultado = document.createElement("div")
-    Div_resultado.setAttribute("id", "Div_Resultado")
-    var Div_texto = document.createTextNode(`O valor do preço 
-    antes da aplicação do desconto era de ${Valor_original}`)
-
+    Div_resultado.setAttribute("id", "Div_resultado")
+    var Div_texto = document.createTextNode(VerificaValorNulo(Valor_original))
     Div_resultado.appendChild(Div_texto)
+
     Section.appendChild(Div_resultado)
 
-
 })
+
+function EliminaDivSobrando(Div_sobrando){
+
+    if(Section.contains(Div_sobrando)){
+        Div_sobrando.remove()
+        console.log("Div detectada")
+    }
+
+}
+
+function VerificaValorNulo(Valor_original){
+
+    var Valor_pre = Input_pre.value
+    var Valor_des = Input_desc.value
+
+    if((Valor_pre||Valor_des=="Insira o preço"||"Insira o desconto") || (Valor_original==0)){
+        console.log(Valor_pre)
+        console.log(Valor_des)
+        console.log(Valor_original)
+        return "Insira um valor válido"
+    } else {
+        return `O valor do preço antes da aplicação do desconto era de ${Valor_original}`
+    }
+
+}
